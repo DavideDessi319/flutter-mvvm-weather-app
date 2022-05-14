5ab8e69ebdaa15ffc4b950f7f2e957ade6df2e2f
evetechsolutions <73545081+evetechsolutions@users.noreply.github.com>
2022-05-14 09:31:53 +0200

    Create date utils extension

--- lib/utils/custom_date_utils.dart
+++
@@ -1,9 +0,0 @@
-import 'package:intl/intl.dart';
-
-class CustomDateUtils {
-  static String dateToFormattedString(DateTime date) {
-    final DateFormat dateFormatter = DateFormat('EEEE, MMM d, hh:mm a');
-    final String formattedDate = dateFormatter.format(date);
-    return formattedDate;
-  }
-}
---
+++ lib/utils/date_utils_extension.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+extension DateUtilsExtension on DateUtils {
+  static String timestampToFormattedString(int timestamp) {
+    final DateFormat dateFormatter = DateFormat('EEEE, MMM d, hh:mm a');
+    DateTime date = DateTime.fromMillisecondsSinceEpoch(timestamp * 1000);
+    final String formattedDate = dateFormatter.format(date);
+    return formattedDate;
+  }
+
+  static String timestampToDayOfWeek(int timestamp) {
+    final DateFormat dateFormatter = DateFormat('EEEE');
+    DateTime date = DateTime.fromMillisecondsSinceEpoch(timestamp * 1000);
+    final String formattedDate = dateFormatter.format(date);
+    return formattedDate;
+  }
+}
