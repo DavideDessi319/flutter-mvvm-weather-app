4539c6009fc55084040d76d6af9b2ebc3a2d1363
evetechsolutions <73545081+evetechsolutions@users.noreply.github.com>
2022-05-17 12:13:46 +0200

    create .env file and integrate env variables inside constants

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Constants {
-  static const OPEN_WEATHER_API_KEY = '42c895268ae4184b9a894f9bf80fcff3';
-  static const OPEN_WEATHER_CURRENT_URL =
-      'https://api.openweathermap.org/data/2.5/weather?lat=51.507351&lon=-0.127758&appid=42c895268ae4184b9a894f9bf80fcff3&units=metric';
-  static const OPEN_WEATHER_FORECAST_URL =
-      'https://api.openweathermap.org/data/2.5/forecast?lat=51.507351&lon=-0.127758&appid=42c895268ae4184b9a894f9bf80fcff3&units=metric&cnt=5';
-  static const OPEN_WEATHER_ICON_URL = 'http://openweathermap.org/img/wn/';
-  static const OPEN_WEATHER_ICON_SUFFIX = '@4x.png';
+  static final String? openWeatherAPIKey = dotenv.env['OPENWEATHERMAP_API_KEY'];
+  static final String openWeatherCurrentWeatherURL =
+      dotenv.env['OPENWEATHERMAP_CURRENT_WEATHER_URL']! + openWeatherAPIKey!;
+  static final String openWeatherForecastURL =
+      dotenv.env['OPENWEATHERMAP_FORECAST_URL']! + openWeatherAPIKey!;
+  static final String? openWeatherIconURL =
+      dotenv.env['OPENWEATHERMAP_ICON_URL'];
+  static const String openWeatherIconSuffix = '@4x.png';
 
   static final Shader headlineGradient = const LinearGradient(
     colors: <Color>[
@@ -26,6 +28,43 @@ class Constants {
       Color(0xFF89BCF1),
       Color(0xFFA4D8F2),
       Color(0xFFB4E7F7),
+      Colors.white
+    ],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+  );
+
+  static const cloudyGradient = LinearGradient(
+    colors: [
+      Color.fromARGB(255, 135, 203, 222),
+      Color.fromARGB(255, 166, 194, 226),
+      Color(0xFFd3e4f8),
+      Color(0xFFf6fafd),
+      Colors.white
+    ],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+  );
+
+  static const rainyGradient = LinearGradient(
+    colors: [
+      Color.fromARGB(255, 152, 176, 208),
+      Color.fromARGB(255, 122, 149, 187),
+      Color(0xFFb0cdef),
+      Color(0xFFd3e4f8),
+      Color(0xFFf6fafd),
+      Colors.white
+    ],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+  );
+
+  static const nightGradient = LinearGradient(
+    colors: [
+      Color(0xFF2B2F77),
+      Color.fromARGB(255, 59, 64, 164),
+      Color.fromARGB(255, 87, 59, 164),
+      Color(0xFF6B4984),
       Colors.white
     ],
     begin: Alignment.topCenter,
